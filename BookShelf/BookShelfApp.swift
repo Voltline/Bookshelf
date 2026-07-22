cc1106d123ac8a03e18308e45f20f0fb225865d9
Voltline <44248193@qq.com>
2026-07-22 10:46:49 +0800

    init: 创建Xcode项目

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Voltline on 2026/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
